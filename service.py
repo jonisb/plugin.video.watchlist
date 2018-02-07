e498fa3b1c1ea0f23cf25fb1a80227188ba6775d
jonisb <github.com@JsBComputing.se>
2018-02-07 07:25:01 +0100

    Start of "service.py"

---
+++ service.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+import xbmc
+
+
+class Monitor(xbmc.Monitor):
+    pass
+
+
+if __name__ == '__main__':
+    monitor = Monitor()
+    try:
+        monitor.waitForAbort()
+    except AttributeError:
+        # Below for Gotham support
+        while not xbmc.abortRequested:
+            xbmc.sleep(500)
+    xbmc.log("Watchlist Service done!", level=xbmc.LOGNOTICE)
